b0e6ed345bd98d8b85d60a3d59f2ec4b61f0e977
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2024-12-04 16:10:27 -0300

    feat: upload cover image

--- src/services/social-feed/bsky.service.ts
+++ src/services/social-feed/bsky.service.ts
@@ -1,33 +1,58 @@
-import { AtpAgent, RichText } from '@atproto/api';
-import { SocialFeedService } from '../../contracts/social-feed.service';
+import { Agent, CredentialSession, RichText } from '@atproto/api';
+import type { SocialFeedService } from '../../contracts/social-feed.service';
 
 export class BskyService implements SocialFeedService {
-  agent: AtpAgent;
-
-  constructor(private readonly config: BskyConfig) {
-    this.agent = new AtpAgent({
-      service: config.service,
-    });
-  }
+  constructor(private readonly config: BskyConfig) {}
 
   async post(message: string, coverUrl?: string): Promise<void> {
-    await this.agent.login({
+    const session = new CredentialSession(new URL(this.config.service));
+    await session.login({
       identifier: this.config.username,
       password: this.config.password,
     });
 
+    const agent = new Agent(session);
     const richText = new RichText({
       text: message,
     });
+    await richText.detectFacets(agent);
 
-    await richText.detectFacets(this.agent);
-    await this.agent.post({
-      text: richText.text,
-      facets: richText.facets,
-    });
+    if (coverUrl) {
+      const coverImage = await this.fetchCoverImage(coverUrl);
+
+      const { data } = await agent.uploadBlob(coverImage);
+
+      await agent.post({
+        text: richText.text,
+        facets: richText.facets,
+        langs: ['en-US'],
+        embed: {
+          $type: 'app.bsky.embed.images',
+          images: [
+            {
+              alt: 'Cover art',
+              image: data.blob,
+            },
+          ],
+        },
+      });
+    } else {
+      await agent.post({
+        text: richText.text,
+        facets: richText.facets,
+        langs: ['en-US'],
+      });
+    }
 
     console.log(message);
     console.log(coverUrl);
+
+    await session.logout();
+  }
+
+  private async fetchCoverImage(url: string): Promise<Buffer> {
+    const response = await fetch(url);
+    return Buffer.from(await response.arrayBuffer());
   }
 }
 
